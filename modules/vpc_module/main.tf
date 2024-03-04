2384dbcd0832ba4a2deb01f67e166282df8e5983
ChinaraMT <153763614+ChinaraMT@users.noreply.github.com>
2024-03-03 23:03:57 -0600

    Update main.tf

--- modules/vpc_module/main.tf
+++ modules/vpc_module/main.tf
@@ -4,3 +4,19 @@ resource "aws_vpc" "vpc" {
     Name = var.vpc_tag
  }
 }
+resource "aws_vpc" "vpc" {
+  cidr_block = var.cidr_block
+
+  tags = {
+    Name = var.vpc_tag
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  count = var.create_attach_igw ? 1 : 0
+
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "${var.vpc_tag}_igw"
+  }
+}
