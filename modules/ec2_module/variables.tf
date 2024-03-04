0fe5b72aa7200b5a518821aa136f0099d5bc3563
ChinaraMT <153763614+ChinaraMT@users.noreply.github.com>
2024-03-03 19:01:24 -0600

    Update variables.tf

--- modules/ec2_module/variables.tf
+++ modules/ec2_module/variables.tf
@@ -1,27 +1,27 @@
-
 variable "ami" {
-  
+  type = string
 }
+
+variable "instance_type" {
+  type = string
+}
+
 variable "key_name" {
-
+  type = string
 }
 
-variable "instance_type"  {
-
+variable "vpc_security_group_ids" {
+  type = list(string)
 }
-
 
 variable "subnet_id" {
-
-}
-
-variable "security_group_id" {
-}
-
-variable "instance_tag" {
-  
+  type = string
 }
 
 variable "user_data" {
+  type = string
+}
 
+variable "instance_tag" {
+  type = string
 }
