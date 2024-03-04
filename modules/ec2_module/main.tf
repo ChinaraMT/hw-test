200347f1f5974471eaf47df071b043231b808324
ChinaraMT <153763614+ChinaraMT@users.noreply.github.com>
2024-03-03 19:00:18 -0600

    Update main.tf

--- modules/ec2_module/main.tf
+++ modules/ec2_module/main.tf
@@ -1,14 +1,12 @@
 resource "aws_instance" "instance" {
-  
-  ami           = var.ami
-  key_name = var.key_name
-  instance_type = var.instance_type 
-  subnet_id     = var.subnet_id
-  security_groups = var.security_group_id
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = var.vpc_security_group_ids
+  subnet_id              = var.subnet_id
+  user_data              = var.user_data
 
   tags = {
     Name = var.instance_tag
-  } 
-
-user_data = var.user_data
+  }
 }
