fdc69facddfccdf3e03adf68e53b59430ff3a0c8
Chinara Meirbekova <chinaramt@Chinaras-MacBook-Pro.local>
2024-03-03 19:38:52 -0600

    modeles updated

--- modules/tg_attach_module/main.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_lb_target_group_attachment" "tgrp_to_lb" {
-  
-  count            = length(var.instance)
-  target_group_arn = var.target_group_arn
-  target_id      = var.instance[count.index]
-}
--- modules/tg_attach_module/output.tf
+++
@@ -1,5 +0,0 @@
-output "id" {
-    value = aws_lb_target_group_attachment.tgrp_to_lb
-}
-
-
