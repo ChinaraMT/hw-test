97e183f0251f97715e23fd37095f66be16b64774
Chinara Meirbekova <chinaramt@Chinaras-MacBook-Pro.local>
2024-03-03 19:21:02 -0600

    module updated

--- modules/igw_module/main.tf
+++ modules/igw_module/main.tf
@@ -1,9 +1,10 @@
 resource "aws_internet_gateway" "igw" {
-  vpc_id = var.vpc_id
+  
 
 count = var.create_attach_igw ? 1 : 0
 
   vpc_id = aws_vpc.vpc.id
+
   tags = {
     Name = "${var.vpc_tag}_igw"
   }
